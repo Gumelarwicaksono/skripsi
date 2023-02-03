1fae0da44160828c21a22336df92fa2d77032355
gumelar <gumelar.wicaksono6@gmail.com>
2023-02-03 09:35:31 +0700

    create skripsi program web

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let sidebar = document.querySelector('.sidebar');
 let searchBtn = document.querySelector('.bx-search');
 
 btn.onclick = function () {
-  sidebar.classList.toggle('active');
+  if (sidebar.classList.contains('active')) {
+    sidebar.classList.remove('active');
+  } else {
+    sidebar.classList.add('active');
+  }
 };
+
 searchBtn.onclick = function () {
   sidebar.classList.toggle('active');
 };
